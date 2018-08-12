80a896aa99424e8adcb9787e405ecc9fcd6c2834
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2018-08-12 11:57:58 +0300

    Revert "add lil tests"
    This reverts commit 2574af85dcf8da57ec740d90e28cf91cc4aacd9b.

--- ml-app/src/algorithms/Algo.h
+++ ml-app/src/algorithms/Algo.h
@@ -5,20 +5,22 @@
 using namespace algorithm_helpers;
 
 
+
 namespace algorithms
 {
-
 
 class Algo
 {
 public:
-	void Fit(const DataFrame& df);
-
-	vector <long double> Predict(const DataFrame& df) const;
+	void Fit(const DataFrame& df)
+	{
+		m_featureNames = df.GetFeatureNames();
+		Learn(df);
+	}
 
 	virtual void Learn(const DataFrame&) = 0;
 
-	virtual void PredictForRange(const DataFrame& df, int start, int end, vector<long double>& outAns) const = 0;
+	virtual vector <long double> Predict(const DataFrame&) const = 0;
 
 	virtual string GetDescriptionOfModel() const = 0;
 
